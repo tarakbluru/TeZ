d03f39703d5f31004d5653b4fdbf7aded3502c60
tarakesh.nc <tarakesh.nc@gmail.com>
2024-01-18 13:48:30 +0530

    Update tez.ps1

--- ps_scripts/tez.ps1
+++ ps_scripts/tez.ps1
@@ -1,5 +1,7 @@
 # Activate virtual environment (Windows)
+Write-Host "Activating Virtual Environment.."
 .\venv\Scripts\Activate.ps1
 
 # Run the Python script within the virtual environment
+Write-Host "Executing TeZ app in the virtual Environment.."
 & ".\venv\Scripts\python.exe" tez_main.py
