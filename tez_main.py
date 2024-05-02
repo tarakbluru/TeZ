89e46dc7423fa640e7eb6c3e89a141e39756ea6a
tarakesh.nc <tarakesh.nc@gmail.com>
2024-05-02 18:04:37 +0530

    Update tez_main.py

--- tez_main.py
+++ tez_main.py
@@ -20,7 +20,7 @@ __email__ = "tarakesh.nc_at_google_mail_dot_com"
 __license__ = "MIT"
 __maintainer__ = "Tarak"
 __status__ = "Development"
-__version__ = "0.7.0_TC6"
+__version__ = "0.7.0_TC7"
 
 import sys
 import traceback
